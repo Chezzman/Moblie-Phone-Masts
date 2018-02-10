936732d0262d9ad42e76c5b3f701bb3b1d9e79b4
Chezzman <gamingmother@gmail.com>
2018-02-10 14:56:12 +0000

    Migrated model to test data

--- mobile_masts/mobile_masts/settings.py
+++ mobile_masts/mobile_masts/settings.py
@@ -19,7 +19,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'web_app.apps.WebAppConfig'
+    'web_app.apps.WebAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- mobile_masts/web_app/models.py
+++ mobile_masts/web_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-#Making a class for Mobile masts Property data from the CSV, to potentiall add data downt the line 
+#Making a class for Mobile masts Property data from the CSV, to potentiall add data downt the line
 class Property(models.Model):
     property_name = models.CharField(max_length=100)
     property_address_line_1 = models.CharField(max_length=50)
@@ -9,7 +9,7 @@ class Property(models.Model):
     property_address_line_4 = models.CharField(max_length=50)
     unit_name = models.CharField(max_length=50)
     tenant_name = models.CharField(max_length=50)
-    lease_start_date = models.CharFieldDateField()
+    lease_start_date = models.DateField()
     lease_end_date = models.DateField()
     lease_years = models.IntegerField()
     current_rent = models.IntegerField()
