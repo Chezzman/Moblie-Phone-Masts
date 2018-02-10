6a53882987f88609b584cb304cc88881c2d388f3
Chezzman <gamingmother@gmail.com>
2018-02-10 14:36:30 +0000

    added a new urls page in web app to keep code in scope, adding layout on html and fake data

--- mobile_masts/mobile_masts/urls.py
+++ mobile_masts/mobile_masts/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from django.urls import re_path
 from django.urls import path
 from web_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^$', views.index, name='index'),
+    path('web_app/', include('web_app.urls')),
 ]
