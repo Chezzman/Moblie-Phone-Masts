f7bc28b24f9eddc2812779bd8f2b877daa773395
Chezzman <gamingmother@gmail.com>
2018-02-14 23:38:38 +0000

    Adding the end point to urls .py

--- mobile_masts/web_app/urls.py
+++ mobile_masts/web_app/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('create/', views.create, name='create'),
     path('date_list', views.date_list, name='date_list'),
     path('table_five', views.table_five, name='table_five'),
-    path('total_rent/', views.total_rent, name='total_rent')
+    path('total_rent/', views.total_rent, name='total_rent'),
+    path('dictionary/' views.dictionary, name='dictionary')
 
 ]
