0a30c73b47407b774c1747d5f76695474337b071
Chezzman <gamingmother@gmail.com>
2018-02-15 09:35:28 +0000

    syntax

--- mobile_masts/web_app/urls.py
+++ mobile_masts/web_app/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('date_list', views.date_list, name='date_list'),
     path('table_five', views.table_five, name='table_five'),
     path('total_rent/', views.total_rent, name='total_rent'),
-    path('dictionary/' views.dictionary, name='dictionary')
+    path('dictionary/', views.dictionary, name='dictionary')
 
 ]
